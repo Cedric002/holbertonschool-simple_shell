7aeb09bc715eaa9ab6df15539f6adcac0ff9ace9
= <giritekaaarsene@gmail.com>
2023-12-21 06:00:21 -0800

    adding _free.c file

---
+++ _free.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _free - free a string
+ * @str: string to free
+ * Return: nothing
+ */
+void _free(char **str)
+{
+	if (*str != NULL)
+	{
+		free(*str);
+		*str = NULL;
+	}
+}
+
+/**
+ * _free_arr - frees array of string
+ * @str_arr: array of str
+ * Return: nothing
+ */
+void _free_arr(char ***str_arr)
+{
+	size_t loop_counter = 0;
+
+	if (*str_arr != NULL)
+	{
+		while ((*str_arr  + loop_counter) != NULL &&
+				*(*str_arr + loop_counter) != NULL)
+		{
+			free(*(*str_arr + loop_counter));
+			loop_counter++;
+		}
+		free(*str_arr);
+		*str_arr = NULL;
+	}
+}
